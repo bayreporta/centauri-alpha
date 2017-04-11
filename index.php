fde423c4e8ca98569965d431d879c3a62ddb86d8
bayreporta <bayreporta@gmail.com>
2017-04-11 14:33:25 -0700

    eliminate author.php, cleaned up attachment.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 				<?php add_filter( 'get_the_archive_title', function ($title) {
 				    if ( is_category() ) {$title = single_cat_title( '', false );} 
 				    elseif ( is_tag() ) {$title = single_tag_title( '', false );} 
-				    elseif ( is_author() ) {$title = '<span class="vcard">' . get_the_author() . '</span>' ;}
+				    elseif ( is_author() ) {$title = 'Everything by John Osborn D\'Agostino';}
 				    return $title;
 				});
 				$title = get_the_archive_title();
