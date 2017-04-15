34d715720b1f14f5042b5f1c1dd96135b70fea7d
John Osborn <bayreporta@gmail.com>
2017-04-14 19:01:15 -0700

    foundation of work index pages setup

---
+++ templates/entry/entry-primary_index.php
@@ -0,0 +1,13 @@
+<section class="main-image">
+	<div role="image"><img alt="" src=""></div>
+	<div role="main-head">
+		<h1 class="aligncenter">
+			<?php print get_field( 'ca_main_header' ); ?>		
+		</h1>
+	</div>
+	<div role="second-head">
+		<h2 class="aligncenter">
+			<?php print get_bloginfo( 'description' ); ?>	
+		</h2>
+	</div>	
+</section>
--- templates/page-index.php
+++ templates/page-index.php
@@ -1,28 +1,41 @@
 <?php /* Template Name: Index */ get_header(); ?>
 
 <!-- Featured image area -->
-<?php get_template_part( 'templates/entry/entry', 'main_image' ) ?>
+<?php //get_template_part( 'templates/entry/entry', 'main_image' ) ?>
 
 <main id="content" role="home">
 
-	<!-- Services area -->
-	<?php get_template_part( 'templates/entry/entry', 'service_list' ) ?>
+	<!-- Header area -->
+	<?php //get_template_part( 'templates/entry/entry', 'service_list' ) ?>
 
-	<!-- Testimonials area -->
-	<?php //get_template_part( 'templates/entry/entry', 'testimonials' ) ?>
+	<!-- Primary Index area -->
+	<?php //get_template_part( 'templates/entry/entry', 'primary_index' ) ?>
 
-	<!-- Latest Blog area -->
-	<?php get_template_part( 'templates/entry/entry', 'latest_post' ) ?>
-	
-	<!-- Clips area -->
-	<?php get_template_part( 'templates/entry/entry', 'clips' ) ?>
+	<!-- Featured Clips area -->
+	<?php //get_template_part( 'templates/entry/entry', 'clips' ) ?>
 
-	<!-- Bio area -->
-	<?php get_template_part( 'templates/entry/entry', 'bio' ) ?>
-
-	<!-- Contact area -->
-	<?php get_template_part( 'templates/entry/entry', 'contact' ) ?>
-	
+	<!-- Secondary Index area -->
+	<div class="entry-segment" role="sec-index">
+		<?php 
+			$cat_id = get_field( 'ca_index_query_category' );
+			$args = array(
+					'post_type'     => array( 'post' ),
+					'post_status'   => array( 'publish'),
+					//'cat'			=> $cat_id,
+					'category_name' => 'bloggin',
+					'cache_results' => true
+		);
+		$query = new WP_Query( $args );
+		centalpha_localize_ajax_script( $args );		
+		while ($query->have_posts()) : $query->the_post();?>			
+			<?php get_template_part( 'templates/entry/entry' , 'index' ); ?>
+		<?php endwhile; ?>	
+		<?php if ( sizeof( $query->posts ) > 9 ) : ?>
+			<div class="button-container">
+				<a href="#" class="index-button" data-page="2"><div class="button">load more posts <i class="fa fa-refresh" aria-hidden="true"></i></div></a>
+			</div>
+		<?php endif; ?>
+	</div>
 </main>
 
 <!-- main image script -->
