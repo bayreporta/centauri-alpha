3b71d34f74b8dd22d70bf618427be6efed3cfe70
John Osborn <bayreporta@gmail.com>
2017-04-16 22:09:59 -0700

    last bit fixes

--- templates/directory/directory-table.php
+++ templates/directory/directory-table.php
@@ -12,5 +12,14 @@
 		<tbody>
 			<?php print centalpha_populate_directory_table(); ?>
 		</tbody>
+		<tfoot>
+			<tr>
+				<th role="icons"></th>
+				<th role="year">Year</th>
+				<th role="name">Game</th>
+				<th role="publish">Publisher</th>
+				<th role="develop">Creator</th>
+			</tr>
+		</tfoot>
 	</table>
 </section>
